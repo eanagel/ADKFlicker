a32b739ddd343c2256fa632d612771146d0c353d
Ethan Nagel <eanagel@gmail.com>
2015-06-25 09:56:54 -0700

    simple test harness for isAfter

--- ADKFlicker/ViewController.m
+++ ADKFlicker/ViewController.m
@@ -16,7 +16,7 @@
 @interface ViewController () <ASTableViewDataSource, ASTableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet ASTableView *tableView;
-@property (nonatomic) NSMutableArray *messages;
+@property (nonatomic) NSMutableArray *sections;
 @property (nonatomic,readonly) NSArray *testData;
 
 @end
@@ -35,21 +35,103 @@
 
   self.title = @"ADK Cell Flicker Test 2.0";
 
-  self.messages = [[self testData] mutableCopy];
+  [self loadData];
 
   self.tableView.asyncDelegate = self;
   self.tableView.asyncDataSource = self;
 
-  self.messages = [[NSMutableArray alloc] init];
+  [self.tableView reloadData];
 
-  for (int x=0; x<25; x++) {
-    [self.messages addObject:[self randomMessage]];
+  self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Test" style:UIBarButtonItemStylePlain target:self action:@selector(testAction:)];
+}
+
+
+- (void)testAction:(id)sender {
+
+  BOOL (^isAfter)(NSIndexPath *, NSIndexPath *) = ^BOOL(NSIndexPath *indexPath, NSIndexPath *anchor) {
+    if (!anchor || !indexPath) {
+      return NO;
+    }
+    if (indexPath.section == anchor.section) {
+      return (indexPath.row == anchor.row+1); // assumes that indexes are valid
+
+    } else if (indexPath.section > anchor.section && indexPath.row == 0) {
+      if (anchor.row != [self.tableView numberOfRowsInSection:anchor.section] -1) {
+        return NO;  // anchor is not at the end of the section
+      }
+
+      NSInteger nextSection = anchor.section+1;
+      while([self.tableView numberOfRowsInSection:nextSection] == 0) {
+        ++nextSection;
+      }
+
+      return indexPath.section == nextSection;
+    }
+
+    return NO;
+  };
+
+  BOOL (^isBefore)(NSIndexPath *, NSIndexPath *) = ^BOOL(NSIndexPath *indexPath, NSIndexPath *anchor) {
+    return isAfter(anchor, indexPath);
+  };
+
+  NSMutableArray *indexPaths = [NSMutableArray array];
+
+  for(int sectionIndex=0; sectionIndex<[self.tableView numberOfSections]; sectionIndex++) {
+    for(int rowIndex=0; rowIndex<[self.tableView numberOfRowsInSection:sectionIndex]; rowIndex++) {
+      [indexPaths addObject:[NSIndexPath indexPathForRow:rowIndex inSection:sectionIndex]];
+    }
   }
 
-  [self.tableView reloadData];
+  for(int anchorIndex=0; anchorIndex<indexPaths.count; anchorIndex++) {
+    for(int itemIndex=0; itemIndex<indexPaths.count; itemIndex++) {
+      NSIndexPath *anchor = indexPaths[anchorIndex];
+      NSIndexPath *item = indexPaths[itemIndex];
+
+      BOOL expected, actual;
+
+      expected = (anchorIndex + 1) == itemIndex;
+      actual = isAfter(item, anchor);
+
+      if (expected != actual) {
+        NSLog(@"isAfter(%@, %@) Test Failed!", item, anchor);
+        actual = isAfter(item, anchor);
+      }
+
+      expected = (anchorIndex - 1) == itemIndex;
+      actual = isBefore(item, anchor);
+
+      if (expected != actual) {
+        NSLog(@"isBefore(%@, %@) Test Failed!", item, anchor);
+        actual = isBefore(item, anchor);
+      }
+    }
+  }
+
 }
 
 #pragma mark - Data
+
+- (void)loadData {
+  NSMutableArray *sections = [[NSMutableArray alloc] init];
+
+  NSArray *rowCounts = @[@(3), @(5), @(1), @(2), @(0), @(5)];
+
+  for (int sectionIndex=0; sectionIndex<rowCounts.count; sectionIndex++) {
+    int numRows = [rowCounts[sectionIndex] intValue];
+
+    NSMutableArray *rows = [NSMutableArray array];
+
+    for(int rowIndex=0; rowIndex<numRows; rowIndex++) {
+      [rows addObject:[self randomMessage]];
+    }
+
+    [sections addObject:@{@"title": [NSString stringWithFormat:@"Section %d", sectionIndex],
+                               @"rows": rows}];
+  }
+
+  self.sections = sections;
+}
 
 - (NSArray *)testData {
   if (!_testData) {
@@ -63,42 +145,33 @@
   return self.testData[arc4random() % self.testData.count];
 }
 
-- (void)toggleLikeAtIndexPath:(NSIndexPath *)indexPath {
-    NSString *likeMessage = @"\n----------\nYou Like This!";
-    NSString *message = self.messages[indexPath.row];
-
-    if ( [message hasSuffix:likeMessage] ) {
-        message = [message substringToIndex:message.length - likeMessage.length];
-    } else {
-        message = [message stringByAppendingString:likeMessage];
-    }
-
-    self.messages[indexPath.row] = message;
-}
-
 
 #pragma mark - ASTableViewDataSource/Delegate
 
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return self.sections.count;
+}
+
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return self.messages.count;
+  NSMutableArray *rows =  [self.sections[section] valueForKey:@"rows"];
+  return rows.count;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+  return [self.sections[section] valueForKey:@"title"];
 }
 
 -(ASCellNode *)tableView:(ASTableView *)tableView nodeForRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSMutableArray *rows =  [self.sections[indexPath.section] valueForKey:@"rows"];
+  NSString *message = rows[indexPath.row];
+
   ASTextCellNode *node = [[ASTextCellNode alloc] init];
-  node.text = self.messages[indexPath.row];
+  node.text = message;
   node.backgroundColor = (indexPath.row%2) ? [[UIColor redColor] colorWithAlphaComponent:0.25] : [[UIColor greenColor] colorWithAlphaComponent:0.25];
 
   return node;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-
-    [self.tableView beginUpdates];
-
-    [self toggleLikeAtIndexPath:indexPath];
-    [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    
-    [self.tableView endUpdates];
-}
 
 @end
